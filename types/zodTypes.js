8629ec8e8dca04cc1f45b0071cf6050dbdfc21b1
Aryan Chouhan <97756708+aryan-c02@users.noreply.github.com>
2024-01-26 19:05:03 +0530

    added-optimized-zod-types

--- types/zodTypes.js
+++ types/zodTypes.js
@@ -5,20 +5,7 @@ const EmailType = z.object({
 })
 
 const IpAddressType = z.object({
-  ip: z.refine((data) => {
-    const ipv4Regex = /^(\d{1,3}\.){3}\d{1,3}$/;
-    const ipv6Regex = /^([\da-f]{1,4}:){7}[\da-f]{1,4}$/i;
-    if (ipv4Regex.test(data)) {
-      return data.split('.').every(part => parseInt(part) <= 255);
-    }
-    if (ipv6Regex.test(data)) {
-      return data.split(':').every(part => part.length <= 4);
-    }
-    return false;
-
-  }, {
-    message: "Invalid Ip address",
-  })
+  ip: z.string().ip({ message: "Invalid IP address" }),
 })
 
 module.exports = { EmailType, IpAddressType }
